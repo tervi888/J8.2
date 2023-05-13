3a4ac64da2d371c16e8b55371398bb663c5744a6
Tervi test <Tervi@list.ru>
2023-05-13 10:41:40 +0300

    set up jacoco

--- src/test/java/ru/netology/statistic/StatisticsServiceTest.java
+++ src/test/java/ru/netology/statistic/StatisticsServiceTest.java
@@ -15,4 +15,15 @@ public class StatisticsServiceTest {
 
         Assertions.assertEquals(expected, actual);
     }
+    @Test
+    void findMax2() {
+        StatisticsService service = new StatisticsService();
+
+        long[] incomesInBillions = {12, 55, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 55;
+
+        long actual = service.findMax(incomesInBillions);
+
+        Assertions.assertEquals(expected, actual);
+    }
 }
